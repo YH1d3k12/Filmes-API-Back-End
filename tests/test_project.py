df54af2b27b43839dc3a1ec271eea2c14eec29ce
djalma <djalmahy@hotmail.com>
2023-10-16 13:42:25 -0300

    Fix: tests test_get_movies

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,8 +1,10 @@
 import unittest
+import json
 from flask import Flask
 
 from src.configs.database import db
 from src.configs.test_configs import TestConfigs
+from src.repositories.movies_repository import MoviesRepository
 from src.routes.movies_route import movie_router
 
 class TestApp(unittest.TestCase):
@@ -21,6 +23,8 @@ class TestApp(unittest.TestCase):
 
         with self.app.app_context():
             db.create_all()
+            
+            MoviesRepository().populate_database()
 
 
     def tearDown(self):
@@ -29,12 +33,43 @@ class TestApp(unittest.TestCase):
             db.drop_all()
 
 
-    def test_example(self):
+    def test_get_movies(self):
         with self.app.test_client() as client:
             response = client.get('/movies/')
             
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.content_type, 'application/json')
+
+            try:
+                json_data = json.loads(response.data)
+            except json.JSONDecodeError:    
+                self.fail('A resposta não é um JSON válido.')
+
+
+            # Assert true if JSON data has message and data keys.
+            self.assertTrue('message' in json_data)
+            self.assertTrue('data' in json_data)
+
+            # Assert true if data is a list.
+            self.assertTrue(isinstance(json_data['data'], list))
+
+            # If at least one movie is within data assert True.
+            self.assertTrue(len(json_data['data']) > 0)
+
+            # Adapte isso para verificar os detalhes específicos do seu JSON.
+            expected_movie = {
+                "id": 1,
+                "year": 1980,
+                "title": "Can't Stop the Music",
+                "studios": "Associated Film Distribution",
+                "producers": "Allan Carr",
+                "winner": "yes"
+            }
+
+            # Verifies if the first movie has the expected_movie values.
+            for key, value in expected_movie.items():
+                self.assertEqual(json_data['data'][0][key], value)
+                print(f'{key} - {value}')
 
 
 if __name__ == '__main__':
